2a1756ff9c5a15cc161f7f23a44b0165c6d80fc9
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-06-15 16:04:36 +0200

    simplify a couple of things

--- app/src/main/java/me/jorgecastillo/viper/common/data/local/InMemoryPhotosDataSource.kt
+++ app/src/main/java/me/jorgecastillo/viper/common/data/local/InMemoryPhotosDataSource.kt
@@ -1,26 +1,35 @@
 package me.jorgecastillo.viper.common.data.local
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import me.jorgecastillo.viper.common.data.network.PhotosNotFound
 import me.jorgecastillo.viper.common.domain.error.Error
 import me.jorgecastillo.viper.common.domain.model.Photo
 import me.jorgecastillo.viper.common.domain.repository.PhotosLocalDataSource
+import me.jorgecastillo.viper.detail.domain.interactor.PhotoNotFound
 
+/**
+ * This is an in-memory implementation of the photos data source, mostly for simplicity of the
+ * example. It could potentially be a persistence implementation.
+ */
 class InMemoryPhotosDataSource : PhotosLocalDataSource {
 
-  override fun getAll(): Either<Error, List<Photo>> {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  private var photos: List<Photo> = emptyList()
+
+  override fun getAll(): Either<Error, List<Photo>> = if (photos.isEmpty()) PhotosNotFound().left() else photos.right()
 
   override fun getPhoto(photoId: String): Either<Error, Photo> {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val photo = photos.find { it.id == photoId }
+    return photo?.right() ?: PhotoNotFound().left()
   }
 
   override fun save(photos: List<Photo>) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val photosToAdd = photos.filterNot { this.photos.contains(it) }
+    this.photos = this.photos + photosToAdd
   }
 
   override fun save(photo: Photo) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    this.photos = this.photos + if (photos.contains(photo)) listOf() else listOf(photo)
   }
-
 }
--- app/src/main/java/me/jorgecastillo/viper/common/domain/model/Photo.kt
+++ app/src/main/java/me/jorgecastillo/viper/common/domain/model/Photo.kt
@@ -1,7 +1,5 @@
 package me.jorgecastillo.viper.common.domain.model
 
-import java.util.*
-
 data class Photo(val id: String,
                  val url: String,
                  val author: String,
